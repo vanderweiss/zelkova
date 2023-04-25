955d5d5ca91f26be5d6ccd4791ec3e6af2d15d0c
vanderweiss <acertig04@gmail.com>
2023-04-24 23:39:47 -0500

    fix omg

--- examples/test.rs
+++ examples/test.rs
@@ -1,5 +1,15 @@
-use zelkova::interface::Governor;
+use zelkova::{
+    tsr,
+    models::*,
+};
 
 fn main() {
-    let governor = Governor::new();
+    
+    let tensor = tsr![
+        [1, 2, 3, 4],
+        [5, 6, 7, 8],
+        [9, 10, 11, 12],
+    ];
+    
+    println!("{}", tensor.rank);
 }
