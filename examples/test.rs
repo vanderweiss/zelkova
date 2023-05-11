a98a533e428914225ee308dedd33668d19bf4015
vanderweiss <acertig04@gmail.com>
2023-05-10 21:36:04 -0500

    forgor

--- examples/test.rs
+++ examples/test.rs
@@ -3,5 +3,5 @@ use zelkova::{models::*, tsr};
 fn main() {
     let tensor = tsr![[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12],];
 
-    println!("{}", tensor.rank);
+    println!("{}", tensor.order);
 }
