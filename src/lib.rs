2b7eaddfd4130d2b3f87746be195204d6b99ae60
vanderweiss <acertig04@gmail.com>
2023-06-20 17:17:11 -0500

    remove annoying

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,11 @@
 //! A vendor-neutral GPU library that aims to provide a simple and straightforward alternative to
 //! modern frameworks for machine learning and AI-related workload.
 
-#![feature(map_try_insert)]
+//#![feature(const_trait_impl)]
 #![feature(lazy_cell)]
+#![feature(map_try_insert)]
 #![feature(ptr_from_ref)]
+#![allow(non_upper_case_globals)]
 
 pub(crate) mod api;
 pub(crate) mod codegen;
