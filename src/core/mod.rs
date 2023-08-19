c22961a4413b0bab53945bc24f5d4e2b447c10ae
vanderweiss <acertig04@gmail.com>
2023-08-18 20:04:11 -0500

    oops wrong module declaration, fixed

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -1,7 +1,6 @@
 //pub(crate) mod error;
 pub(crate) mod bundle;
 pub(crate) mod instance;
-pub(crate) mod op;
 
 pub(crate) use self::bundle::{Bundle, Operation};
 pub(crate) use self::instance::Instance;
